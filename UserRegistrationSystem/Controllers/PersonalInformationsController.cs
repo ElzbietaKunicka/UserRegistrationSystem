5bb6aee19200c19f9ddbd4c877097ff7630a5b7d
elzzzbieta <elzbieta.petkevic@gmail.com>
2023-02-28 15:49:29 +0200

    Break line

--- UserRegistrationSystem/Controllers/PersonalInformationsController.cs
+++ UserRegistrationSystem/Controllers/PersonalInformationsController.cs
@@ -16,70 +16,97 @@ namespace UserRegistrationSystem.Controllers
     public class PersonalInformationsController : ControllerBase
     {
         private readonly IPersonalInformationList _personalInformationList;
-        public PersonalInformationsController(IPersonalInformationList personalInformationList)
+        public PersonalInformationsController(IPersonalInformationList 
+            personalInformationList)
         {
             _personalInformationList = personalInformationList;
         }
 
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "User, Admin")]
+        [Authorize(AuthenticationSchemes = 
+            JwtBearerDefaults.AuthenticationScheme, 
+            Roles = "User, Admin")]
         [HttpPost]
-        public ActionResult PostItem([FromBody] PersonalInformationDto personalInformationToAdd)
+        public ActionResult PostItem([FromBody] PersonalInformationDto 
+            personalInformationToAdd)
         {
-            var userIdStr = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            var userIdStr = User.Claims
+                .FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)
+                .Value;
             var currentUserIdInt = int.Parse(userIdStr);
             if (ModelState.IsValid)
             {
-                _personalInformationList.AddNewPersonalInformation(currentUserIdInt, personalInformationToAdd);
+                _personalInformationList
+                    .AddNewPersonalInformation(currentUserIdInt, 
+                    personalInformationToAdd);
                 return Ok();
             }
             return BadRequest(ModelState);
             //_personalInformationList.AddNewPersonalInformation(currentUserIdInt, personalInformationToAdd);
         }
 
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "User, Admin")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults
+            .AuthenticationScheme, Roles = "User, Admin")]
         [HttpPut]
-        public void UpdateItem([FromBody] PersonalInformationDto personalInformationToAdd)
+        public void UpdateItem([FromBody] PersonalInformationDto 
+            personalInformationToAdd)
         {
-            var userIdStr = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            var userIdStr = User.Claims
+                .FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)
+                .Value;
             var currentUserIdInt = int.Parse(userIdStr);
-            _personalInformationList.UpdatePersonalInformation(currentUserIdInt, personalInformationToAdd);
+            _personalInformationList.UpdatePersonalInformation(currentUserIdInt, 
+                personalInformationToAdd);
         }
 
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "User, Admin")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults
+            .AuthenticationScheme, Roles = "User, Admin")]
         [HttpGet("PersonalInformationId")]
         public int? GetPersonalInformationIdByCurrentUserId()
         {
-            var userIdStr = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            var userIdStr = User.Claims
+                .FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)
+                .Value;
             var currentUserIdInt = int.Parse(userIdStr);
-            return _personalInformationList.getPersonalInformationIdByCurrentUser(currentUserIdInt);
+            return _personalInformationList
+                .getPersonalInformationIdByCurrentUser(currentUserIdInt);
         }
 
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "User, Admin")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults
+            .AuthenticationScheme, Roles = "User, Admin")]
         [HttpGet("AccountsIdAndUsernames")]
         public IEnumerable<AccountDto> GetUsersIdAndUsernames()
         {
             return _personalInformationList.GetUsersIdAndUsernames();
         }
         
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "User, Admin")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults
+            .AuthenticationScheme, Roles = "User, Admin")]
         [HttpGet("CurrentUserInfo")]
         public PersonalInformation GetAllInfoAboutCurrentUser()
         {
-            var userIdStr = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            var userIdStr = User.Claims
+                .FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)
+                .Value;
             var currentUserIdInt = int.Parse(userIdStr);
-            return _personalInformationList.GetAllInfoAboutCurrentUser(currentUserIdInt);
+            return _personalInformationList
+                .GetAllInfoAboutCurrentUser(currentUserIdInt);
         }
 
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "User, Admin")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults
+            .AuthenticationScheme, Roles = "User, Admin")]
         [HttpGet("CurrentUserName")]
         public string GetCurrentUserName()
         {
-            var userIdStr = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            var userIdStr = User.Claims
+                .FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)
+                .Value;
             var currentUserIdInt = int.Parse(userIdStr);
-            return _personalInformationList.GetCurrentUserName(currentUserIdInt);
+            return _personalInformationList
+                .GetCurrentUserName(currentUserIdInt);
         }
 
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "User, Admin")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults
+            .AuthenticationScheme, Roles = "User, Admin")]
         [HttpGet("GetById/{id?}")]
         public ActionResult GetInformationByID(int id)
         {
@@ -91,7 +118,8 @@ namespace UserRegistrationSystem.Controllers
             return Ok(userInfoById);
         }
 
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults
+            .AuthenticationScheme, Roles = "Admin")]
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
